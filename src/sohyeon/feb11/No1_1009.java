2b7642601c772d681b2e2236451a816ffee2af6e
hyeonnnnnnn <so.hyeonn@gmail.com>
2022-02-15 17:09:47 +0900

    1번 3번

--- src/sohyeon/feb11/No1_1009.java
+++ src/sohyeon/feb11/No1_1009.java
@@ -1,5 +1,7 @@
 package sohyeon.feb11;
 
+import java.util.Scanner;
+
 /*
 재용이는 최신 컴퓨터 10대를 가지고 있다.
  
@@ -9,7 +11,7 @@ package sohyeon.feb11;
 
 10번 데이터는 10번 컴퓨터, 11번 데이터는 1번 컴퓨터, 12번 데이터는 2번 컴퓨터, ...
 
-총 데이터의 개수는 항상 ab개의 형태로 주어진다. 재용이는 문득 마지막 데이터가 처리될 컴퓨터의 번호가 궁금해졌다. 이를 수행해주는 프로그램을 작성하라.
+총 데이터의 개수는 항상 a^b개의 형태로 주어진다. 재용이는 문득 마지막 데이터가 처리될 컴퓨터의 번호가 궁금해졌다. 이를 수행해주는 프로그램을 작성하라.
 --
 입력의 첫 줄에는 테스트 케이스의 개수 T가 주어진다. 그 다음 줄부터 각각의 테스트 케이스에 대해 정수 a와 b가 주어진다. (1 ≤ a < 100, 1 ≤ b < 1,000,000)
 --
@@ -33,7 +35,28 @@ public class No1_1009 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("dd");
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();		// 입력받을 수 개수
+		
+		for(int i = 0; i < T; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int result = 1;		// a의 1 제곱을 먼저 구하기 위해 result 1로 초기화
+			
+			for(int j = 0; j < b; j++) {
+				
+				result =  (result * a) % 10;
+				
+				if(result == 0) {		// 마지막 자리수가 0일 때 10번째 컴퓨터
+					result = 10;
+				}
+			}
+			
+			System.out.println(result);
+			
+		}
+		
+		sc.close();
 	}
 
 }
