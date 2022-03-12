9afbd76a01259a2c8d549cfe72028b695ead7dfd
mijin <vlzkcb92326@naver.com>
2022-03-12 12:10:43 +0900

    std

--- src/mijin/std_mijin/y_2022/mar_12/bj_1964.java
+++ src/mijin/std_mijin/y_2022/mar_12/bj_1964.java
@@ -12,7 +12,7 @@ public class bj_1964 {
 		
 		//짝수   5*(1~n까지 합) - (n*(n-1) +(n-1)) //1~n 까지의 합 (n+1)* (n/2)
 		if (n%2==0)
-			System.out.println( (3*n*n)/2 + (5*n)/2+1);
+			System.out.println( ((3*n*n)/2 + (5*n)/2+1)%45678 );
 		else
 		//홀수 5*(1~n까지 합) - (n*(n-1) +(n-1))  // 1~n 까지의 합 (n+1)*(n/2)+(n/2+1)
 			System.out.println((n/2 * (n+1)+n/2+1)*5 - n*n +1);
